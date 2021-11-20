bf4401bdb4bd44348c8645faec71efcb8590e91a
Carlos Navarro <carlosnavarro@agro.unc.edu.ar>
2021-11-20 17:23:33 -0300

    commit 19

--- map.js
+++ map.js
@@ -1,8 +1,21 @@
-let demoMap = L.map('map').setView([40.453191, -3.509236], 6);	
-const osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+var map = L.map('map').setView([-29.8,-60.5], 8); 
+var roads = L.gridLayer
+	.googleMutant({
+		type: "hybrid", // valid values are 'roadmap', 'satellite', 'terrain' and 'hybrid'
+	})
+	.addTo(map);
 
-L.tileLayer(osmUrl, {
-    minZoom: 5,
-    maxZoom: 16,
-}).addTo(demoMap);
+// var osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap<\/a> contributors'
+// }).addTo(map);
 
+function popUpInfo(feature, layer) {
+    // does this feature have a property named popupContent?
+    if (feature.properties && feature.properties.Name) {
+        layer.bindPopup("<b>"+feature.properties.Name+"</b>");
+        // layer.bindPopup(feature.properties.Name);
+    }
+}
+L.geoJson(RedPlu, {
+    onEachFeature: popUpInfo
+    }).addTo(map);
