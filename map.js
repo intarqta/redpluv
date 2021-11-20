4c427231c41c72ccbbc12e7face1988f9b767de7
Carlos Navarro <carlosnavarro@agro.unc.edu.ar>
2021-11-20 17:37:31 -0300

    commit 20

--- map.js
+++ map.js
@@ -1,21 +1,17 @@
+
 var map = L.map('map').setView([-29.8,-60.5], 8); 
-var roads = L.gridLayer
-	.googleMutant({
-		type: "hybrid", // valid values are 'roadmap', 'satellite', 'terrain' and 'hybrid'
-	})
-	.addTo(map);
-
-// var osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-//     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap<\/a> contributors'
-// }).addTo(map);
-
+// Agregar Google Hibrido como mapa base
+var osmLayer =  L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',{
+    maxZoom: 8,
+    subdomains:['mt0','mt1','mt2','mt3']}).addTo(map);
+// Función para generar un cartel al hacer click en el pluviometro
 function popUpInfo(feature, layer) {
-    // does this feature have a property named popupContent?
     if (feature.properties && feature.properties.Name) {
         layer.bindPopup("<b>"+feature.properties.Name+"</b>");
         // layer.bindPopup(feature.properties.Name);
     }
 }
+// Agregar el GeoJson al mapa
 L.geoJson(RedPlu, {
     onEachFeature: popUpInfo
     }).addTo(map);
