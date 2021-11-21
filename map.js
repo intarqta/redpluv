c36fe4adf7492af7f497d6b641033e8634532fee
Carlos Navarro <carlosnavarro@agro.unc.edu.ar>
2021-11-21 08:25:57 -0300

    commit 25

--- map.js
+++ map.js
@@ -14,9 +14,8 @@ var redMarker = L.icon({
 })
 // L.marker([-29.3,-60.4], {icon:redMarker}).addTo(map)
 // Función para generar un cartel al hacer click en el pluviometro
-function popUpInfo(feature, layer) {
+function popUpInfo (feature, layer) {
     if (feature.properties && feature.properties.Name) {
-<<<<<<< HEAD
         layer.bindTooltip(feature.properties.Name, {
             interactive: true,
             permanent: false,
@@ -24,9 +23,7 @@ function popUpInfo(feature, layer) {
             direction: 'top',
             className: 'popup',
         });
-=======
         layer.bindPopup("<b>"+feature.properties.Name+"</b>");
->>>>>>> c0c5a7276abe6b4337bd7396495160aaf7657db9
         layer.setIcon(redMarker)
     }
 }
@@ -40,10 +37,7 @@ L.geoJson(Departamentos, {
 
 // Agregar el GeoJson al mapa
 L.geoJson(RedPlu, {
-<<<<<<< HEAD
     onEachFeature: popUpInfo,
-    fillOpacity:0.1
-=======
+    fillOpacity:0.1,
     onEachFeature: popUpInfo
->>>>>>> c0c5a7276abe6b4337bd7396495160aaf7657db9
 }).addTo(map);
